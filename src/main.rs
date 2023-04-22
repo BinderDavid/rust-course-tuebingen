1cf1027b5dcc949cd04053ee361fdc15264f03ef
David Binder <binderd@informatik.uni-tuebingen.de>
2023-04-22 16:26:22 +0200

    Add version with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,68 @@
 fn main() {
-    let stdin = std::io::stdin();
-    let mut str = String::new();
-
     println!("Enter your weight");
-    stdin.read_line(&mut str).unwrap();
-    let weight = str.trim().parse::<f64>().unwrap();
+    let weight = read_f64();
+
     println!("Enter your height");
-    str.clear();
-    stdin.read_line(&mut str).unwrap();
-    let height = str.trim().parse::<f64>().unwrap();
-    let bmi = calculate_bmi(height, weight);
+    let height = read_f64();
+
+    let (bmi, score) = calculate_bmi(Height(height), Weight(weight));
     println!("Your BMI is: {bmi}");
+    match score {
+        Score::UnderWeight => println!("You are underweight"),
+        Score::NormalWeight => println!("Your weight is in the normal range"),
+        Score::OverWeight => println!("You are overweight"),
+    }
 }
 
-fn calculate_bmi(height: f64, weight: f64) -> f64 {
-    weight / (height * height)
+fn read_f64() -> f64 {
+    let stdin = std::io::stdin();
+    let mut str = String::new();
+    loop {
+        stdin.read_line(&mut str).unwrap();
+        let res = str.trim().parse::<f64>();
+        match res {
+            Ok(res) => return res,
+            Err(_) => {
+                str.clear();
+                println!("Try again.")
+            }
+        }
+    }
+}
+
+#[repr(transparent)]
+struct Height(f64);
+
+#[repr(transparent)]
+struct Weight(f64);
+
+#[derive(PartialEq, Debug)]
+enum Score {
+    UnderWeight,
+    NormalWeight,
+    OverWeight,
+}
+
+/// Calculate the BMI
+fn calculate_bmi(height: Height, weight: Weight) -> (f64, Score) {
+    let bmi = weight.0 / (height.0 * height.0);
+    let score = match bmi {
+        0.0..=18.4 => Score::UnderWeight,
+        18.4..=24.9 => Score::NormalWeight,
+        24.9..=f64::MAX => Score::OverWeight,
+        _ => panic!("invalid bmi"),
+    };
+    (bmi, score)
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn calculate_bmi_1() {
+        assert_eq!(
+            crate::calculate_bmi(crate::Height(2.0), crate::Weight(100.0)),
+            (25.0, crate::Score::OverWeight)
+        )
+    }
 }
