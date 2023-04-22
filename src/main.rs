213e0e1e327998325404f301434f28852f1b944b
David Binder <binderd@informatik.uni-tuebingen.de>
2023-04-22 15:57:37 +0200

    Add initial version of BMI calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let mut str = String::new();
+
+    println!("Enter your weight");
+    stdin.read_line(&mut str).unwrap();
+    let weight = str.trim().parse::<f64>().unwrap();
+    println!("Enter your height");
+    str.clear();
+    stdin.read_line(&mut str).unwrap();
+    let height = str.trim().parse::<f64>().unwrap();
+    let bmi = calculate_bmi(height, weight);
+    println!("Your BMI is: {bmi}");
+}
+
+fn calculate_bmi(height: f64, weight: f64) -> f64 {
+    weight / (height * height)
 }
